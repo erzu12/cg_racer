8b6f4891048f654b4cea769c8dd8239b4456f662
Jonas Fink <jonas.fink@gmx.ch>
2022-12-06 22:27:20 +0100

    cleanup

--- build/hfg/CMakeFiles/hfg.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libhfg.a"
-  "libhfg.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
