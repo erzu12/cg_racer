1f165837ef91118eb734ec44904714b6c7bd86be
Jonas Fink <jonas.fink@stud.hslu.ch>
2024-05-10 17:46:29 +0200

    Doxygen conf
    cmake not working yet

--- src/input.c
+++ src/input.c
@@ -6,6 +6,13 @@ void processInput(Window *window)
         setWindowShouldClose(window, true);
 }
 
+/**
+ * @brief GLFW callback for mouse movement
+ *
+ * @param window The window that received the event
+ * @param posX The new x-coordinate, in screen coordinates, of the cursor
+ * @param posY The new y-coordinate, in screen coordinates, of the cursor
+ */
 void mouse_callback(Window *window, double posX, double posY){
     struct CallbackContext *cbc = (struct CallbackContext *)windowGetUserPointer(window);
     cbc->mousePos.x = (float)posX;
